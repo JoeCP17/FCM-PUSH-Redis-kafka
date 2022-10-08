078ef4189ea6aae4a62da71a0afaf15ff5c5a243
kimueibin <at365@naver.com>
2022-10-08 22:34:04 +0900

    feat:Add to Redis,mysql Docker

---
+++ push-redis/src/main/java/com/push/redis/Application.java
@@ -0,0 +1,12 @@
+package com.push.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"org.push"})
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
